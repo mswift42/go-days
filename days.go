3a671d5806c5464de1835d6a8e24bd844be15250
martin.haesler <martin.haesler@gmail.com>
2014-03-31 20:45:20 +0100

    Delete /sign

--- days.go
+++ days.go
@@ -3,7 +3,6 @@ package days
 import (
         "github.com/codegangsta/martini"
         "github.com/martini-contrib/render"
-        "html/template"
         "net/http"
 )
 
@@ -16,23 +15,4 @@ func init() {
         })
         //      http.HandleFunc("/", root)
         http.Handle("/", m)
-        http.HandleFunc("/sign", sign)
 }
-
-func sign(w http.ResponseWriter, r *http.Request) {
-        err := signTemplate.Execute(w, r.FormValue("content"))
-        if err != nil {
-                http.Error(w, err.Error(), http.StatusInternalServerError)
-        }
-}
-
-var signTemplate = template.Must(template.New("sign").Parse(signTemplateHtml))
-
-const signTemplateHtml = `
-<html>
-  <body>
-    <p>You wrote:</p>
-    <pre>{{.}}</pre>
-  </body>
-</html>
-`
