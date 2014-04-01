4d339393e6dbb5da3815a26cae386ce0136e303e
martin.haesler <martin.haesler@gmail.com>
2014-04-01 17:04:51 +0100

    Add routes /about /newtask

--- days.go
+++ days.go
@@ -4,7 +4,14 @@ import (
         "github.com/codegangsta/martini"
         "github.com/martini-contrib/render"
         "net/http"
+        "time"
 )
+
+type Task struct {
+        Summary   string
+        Scheduled time.Time
+        Done      bool
+}
 
 func init() {
         m := martini.Classic()
@@ -13,6 +20,15 @@ func init() {
         m.Get("/", func(r render.Render) {
                 r.HTML(200, "home", map[string]interface{}{"Pagetitle": "Tasks"})
         })
+        m.Get("/newtask", func(r render.Render) {
+                r.HTML(200, "newtask", map[string]interface{}{"Pagetitle": "New Task"})
+        })
+        m.Get("/about", func(r render.Render) {
+                r.HTML(200, "about", map[string]interface{}{"Pagetitle": "About"})
+        })
         //      http.HandleFunc("/", root)
         http.Handle("/", m)
+        http.Handle("/newtask", m)
+        http.Handle("/about", m)
+
 }
