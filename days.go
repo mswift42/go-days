ab747b2cf9f45e7958236aa66b41c4672d8dfb9e
martin.haesler <martin.haesler@gmail.com>
2014-04-03 20:20:04 +0100

    Add /about

--- days.go
+++ days.go
@@ -3,6 +3,7 @@ package days
 import (
         "appengine"
         "appengine/datastore"
+        "html/template"
         "net/http"
         "time"
 )
@@ -17,5 +18,32 @@ type Task struct {
 // guestbookKey returns the key used for all guestbook entries.
 func tasklistkey(c appengine.Context) *datastore.Key {
         // The string "default_guestbook" here could be varied to have multiple guestbooks.
-        return datastore.NewKey(c, "Tasklist", "default_tasklist", 0, nil)
+        return datastore.NewKey(c, "Task", "default_tasklist", 0, nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+        // home := make(map[string]*template.Template)
+        // homeTmpl["home.tmpl"]
+        homeTmpl := template.Must(template.New("home").ParseFiles("templates/home.tmpl", "templates/layout.tmpl"))
+        c := appengine.NewContext(r)
+        q := datastore.NewQuery("Task").Ancestor(tasklistkey(c)).Order("-Date")
+        tasks := make([]Task, 0, 10)
+        if _, err := q.GetAll(c, &tasks); err != nil {
+                http.Error(w, err.Error(), http.StatusInternalServerError)
+                return
+        }
+        if err := homeTmpl.Execute(w, map[string]interface{}{"Pagetitle": "Tasks",
+                "Tasks": tasks}); err != nil {
+                http.Error(w, err.Error(), http.StatusInternalServerError)
+        }
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+        tmpl := template.Must(template.New("about").ParseFiles("templates/layout.tmpl", "templates/about.tmpl"))
+        tmpl.Execute(w, map[string]interface{}{"Pagetitle": "About"})
+}
+
+func init() {
+        http.HandleFunc("/", home)
+        http.HandleFunc("/about", about)
 }
