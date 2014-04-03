070ff839e53156609e6ba02ee9fb87618e58da65
martin.haesler <martin.haesler@gmail.com>
2014-04-03 18:02:45 +0100

    Delete martini deps, start fresh

--- days.go
+++ days.go
@@ -1,34 +1,21 @@
 package days
 
 import (
-        "github.com/codegangsta/martini"
-        "github.com/martini-contrib/render"
+        "appengine"
+        "appengine/datastore"
         "net/http"
         "time"
 )
 
 type Task struct {
         Summary   string
+        Content   string
         Scheduled time.Time
         Done      bool
 }
 
-func init() {
-        m := martini.Classic()
-        m.Use(render.Renderer(render.Options{Layout: "layout",
-                Directory: "templates"}))
-        m.Get("/", func(r render.Render) {
-                r.HTML(200, "home", map[string]interface{}{"Pagetitle": "Tasks"})
-        })
-        m.Get("/newtask", func(r render.Render) {
-                r.HTML(200, "newtask", map[string]interface{}{"Pagetitle": "New Task"})
-        })
-        m.Get("/about", func(r render.Render) {
-                r.HTML(200, "about", map[string]interface{}{"Pagetitle": "About"})
-        })
-        //      http.HandleFunc("/", root)
-        http.Handle("/", m)
-        http.Handle("/newtask", m)
-        http.Handle("/about", m)
-
+// guestbookKey returns the key used for all guestbook entries.
+func tasklistkey(c appengine.Context) *datastore.Key {
+        // The string "default_guestbook" here could be varied to have multiple guestbooks.
+        return datastore.NewKey(c, "Tasklist", "default_tasklist", 0, nil)
 }
