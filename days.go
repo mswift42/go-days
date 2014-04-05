7538ed5e32a7a9a9e04081cafd83e8ab1b66478d
martin.haesler <martin.haesler@gmail.com>
2014-04-05 22:25:53 +0100

    Add datepicker

--- days.go
+++ days.go
@@ -1,69 +1,73 @@
 package days
 
 import (
-        "appengine"
-        "appengine/datastore"
-        "html/template"
-        "net/http"
-        "time"
+	"appengine"
+	"appengine/datastore"
+	"html/template"
+	"net/http"
+	//	"time"
 )
 
+// Task - struct for datastore table.
+// Contains a summary and the contents of a task, the scheduled
+// time for the task and whether it is done or not.
 type Task struct {
-        Summary   string
-        Content   string
-        Scheduled string
-        Done      bool
+	Summary   string
+	Content   string
+	Scheduled string
+	Done      bool
 }
 
-// guestbookKey returns the key used for all guestbook entries.
 func tasklistkey(c appengine.Context) *datastore.Key {
-        // The string "default_guestbook" here could be varied to have multiple guestbooks.
-        return datastore.NewKey(c, "Task", "default_tasklist", 0, nil)
+	return datastore.NewKey(c, "Task", "default_tasklist", 0, nil)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
-        homeTmpl := template.Must(template.New("home").ParseFiles("templates/home.tmpl", "templates/layout.tmpl"))
-        c := appengine.NewContext(r)
-        q := datastore.NewQuery("Task").Ancestor(tasklistkey(c)).Order("-Scheduled").Limit(10)
-        tasks := make([]Task, 0, 10)
-        if _, err := q.GetAll(c, &tasks); err != nil {
-                http.Error(w, err.Error(), http.StatusInternalServerError)
-                return
-        }
-        if err := homeTmpl.Execute(w, map[string]interface{}{"Pagetitle": "Tasks",
-                "tasks": tasks}); err != nil {
-                http.Error(w, err.Error(), http.StatusInternalServerError)
-        }
+	homeTmpl := template.Must(template.New("home").ParseFiles("templates/home.tmpl",
+		"templates/layout.tmpl"))
+	c := appengine.NewContext(r)
+	q := datastore.NewQuery("Task").Ancestor(tasklistkey(c)).Order("-Scheduled").Limit(10)
+	tasks := make([]Task, 0, 10)
+	if _, err := q.GetAll(c, &tasks); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := homeTmpl.Execute(w, map[string]interface{}{"Pagetitle": "Tasks",
+		"tasks": tasks}); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 func newtask(w http.ResponseWriter, r *http.Request) {
-        newTmpl := template.Must(template.New("newtask").ParseFiles("templates/layout.tmpl", "templates/newtask.tmpl"))
-        if err := newTmpl.Execute(w, map[string]interface{}{"Pagetitle": "New Task"}); err != nil {
-                http.Error(w, err.Error(), http.StatusInternalServerError)
-        }
+	newTmpl := template.Must(template.New("newtask").ParseFiles("templates/layout.tmpl",
+		"templates/newtask.tmpl"))
+	if err := newTmpl.Execute(w, map[string]interface{}{"Pagetitle": "New Task"}); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func storetask(w http.ResponseWriter, r *http.Request) {
-        c := appengine.NewContext(r)
-        t := Task{Summary: r.FormValue("tinput"),
-                Content:   r.FormValue("tarea"),
-                Scheduled: time.Now().Format(time.RFC822)}
-        key := datastore.NewIncompleteKey(c, "Task", tasklistkey(c))
-        _, err := datastore.Put(c, key, &t)
-        if err != nil {
-                http.Error(w, err.Error(), http.StatusInternalServerError)
-                return
-        }
-        http.Redirect(w, r, "/", http.StatusFound)
+	c := appengine.NewContext(r)
+	t := Task{Summary: r.FormValue("tinput"),
+		Content:   r.FormValue("tarea"),
+		Scheduled: r.FormValue("scheduled")}
+	key := datastore.NewIncompleteKey(c, "Task", tasklistkey(c))
+	_, err := datastore.Put(c, key, &t)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/", http.StatusFound)
 }
 
 func about(w http.ResponseWriter, r *http.Request) {
-        tmpl := template.Must(template.New("about").ParseFiles("templates/layout.tmpl", "templates/about.tmpl"))
-        tmpl.Execute(w, map[string]interface{}{"Pagetitle": "About"})
+	tmpl := template.Must(template.New("about").ParseFiles("templates/layout.tmpl",
+		"templates/about.tmpl"))
+	tmpl.Execute(w, map[string]interface{}{"Pagetitle": "About"})
 }
 
 func init() {
-        http.HandleFunc("/", home)
-        http.HandleFunc("/about", about)
-        http.HandleFunc("/storetask", storetask)
-        http.HandleFunc("/newtask", newtask)
+	http.HandleFunc("/", home)
+	http.HandleFunc("/about", about)
+	http.HandleFunc("/storetask", storetask)
+	http.HandleFunc("/newtask", newtask)
 }
