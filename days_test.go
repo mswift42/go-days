f808bc98660787b68d78af5f752c9f83fd3dd28e
martin.haesler <martin.haesler@gmail.com>
2014-04-13 17:39:20 +0100

    Delete 2nd test

--- days_test.go
+++ days_test.go
@@ -7,13 +7,16 @@ import (
 )
 
 func TestTasks(t *testing.T) {
+	t1 := Task{Summary: "task1", Content: "some content", Identifier: "123",
+		Done: false}
 	c, err := aetest.NewContext(nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 	key := datastore.NewKey(c, "Task", "", 1, nil)
-	if _, err := datastore.Put(c, key, &Task{Summary: "test1", Content: "some content"}); err != nil {
+	if _, err := datastore.Put(c, key, &t1); err != nil {
 		t.Fatal(err)
 	}
+
 	defer c.Close()
 }
